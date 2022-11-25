e06b303f12f61591b3ee4f5e5782c64c2c174080
YanaDemchyshyn <115561120+YanaDemchyshyn@users.noreply.github.com>
2022-11-25 17:07:38 +0200

    add function formatDate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+function formatDate(getdate) {
+    let date = new Date(getdate);
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Suterday",
+    ];
+    let day = days[date.getDay()];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let month = months[date.getMonth()];
+    let number = date.getDate();
+    return `${day}, ${month} ${number}`;
+}
 function formatTime(timestamp) {
     let time = new Date(timestamp);
     let hours = time.getHours();
@@ -23,6 +53,8 @@ function displayTemperature(response) {
     )} m/s`;
     let timeElement = document.querySelector(".time");
     timeElement.innerHTML = formatTime(response.data.time * 1000);
+    let dataElement = document.querySelector(".date");
+    dataElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let apiKey = "14b37c21t13746dfa0b52a2b355co69b";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Kyiv}&key=${apiKey}&units=metric`;
